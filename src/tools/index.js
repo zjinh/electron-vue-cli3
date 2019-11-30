73432d951dde46f826c7f867acb94703f938b90f
ZJINH <2665229856@qq.com>
2019-11-30 16:23:46 +0800

    更新

--- src/tools/index.js
+++ src/tools/index.js
@@ -71,6 +71,71 @@ String.prototype.Exist = function(substr) {
   }
   return false;
 };
+Date.prototype.format = function(fmt) {
+  const o = {
+    "y+": this.getFullYear(),
+    "M+": this.getMonth() + 1,
+    "d+": this.getDate(),
+    "H+": this.getHours(),
+    "m+": this.getMinutes(),
+    "s+": this.getSeconds(),
+    "S+": this.getMilliseconds(),
+    "q+": Math.floor(this.getMonth() / 3) + 1,
+    "h+": (() => {
+      const hour = this.getHours() % 12;
+      return hour === 0 ? 12 : hour;
+    })(),
+    "E+": (() => {
+      const week = {
+        "0": "Sunday",
+        "1": "Monday",
+        "2": "Tuesday",
+        "3": "Wednesday",
+        "4": "Thursday",
+        "5": "Friday",
+        "6": "Saturday"
+      };
+      return week[this.getDay() + ""];
+    })(),
+    /*
+    "e+": (()=>{
+        const week = {"0":"Sun","1":"Mon","2":"Tue","3":"Wed","4":"Thu","5":"Fri","6":"Sat"};
+        return week[this.getDay()+""];
+    })(),
+    */
+    x1: (() => {
+      const week = {
+        "0": "周日",
+        "1": "周一",
+        "2": "周二",
+        "3": "周三",
+        "4": "周四",
+        "5": "周五",
+        "6": "周六"
+      };
+      return week[this.getDay() + ""];
+    })(),
+    x2: (() => {
+      const hour = ["凌晨", "早上", "下午", "晚上"];
+      const h = this.getHours();
+      if (h === 12) return "中午";
+      return hour[parseInt(h / 6)];
+    })()
+  };
+  for (let k in o) {
+    if (new RegExp("(" + k + ")", "g").test(fmt)) {
+      const len = RegExp.$1.length;
+      fmt = fmt.replace(
+        RegExp.$1,
+        len === 1 ? o[k] : ("00" + o[k]).substr(-len)
+      );
+    }
+  }
+  return fmt;
+};
+JSON.handle = function(data) {
+  return JSON.parse(JSON.stringify(data));
+};
 
 //引入electron接口
 Vue.path = Vue.prototype.$path = path; //path接口
@@ -81,3 +146,4 @@ Vue.Notify = Vue.prototype.$notify = (msg, name) => {
     body: msg
   });
 }; //通知接口
+/**************************************************************自定义从这开始**************************************************************/
